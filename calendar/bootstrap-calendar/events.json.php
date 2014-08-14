51bf4532e2b64be250790122ff453db18c7a0c36
CamBig <bigelow.cameron@gmail.com>
2014-08-14 11:29:35 -0600

    first commit

---
+++ calendar/bootstrap-calendar/events.json.php
@@ -0,0 +1,47 @@
+{
+	"success": 1,
+	"result": [
+		{
+			"id": "293",
+			"title": "VMUG Webinar Example AM",
+			"url": "https://vmug-events.webex.com/mw0401l/mywebex/default.do?nomenu=true&siteurl=vmug-events&service=6&rnd=0.22240610651555404&main_url=https%3A%2F%2Fvmug-events.webex.com%2Fec0701l%2Feventcenter%2Fevent%2FeventAction.do%3FtheAction%3Ddetail%26confViewID%3D1749628482%26%26MK%3D667500023%26%26%26siteurl%3Dvmug-events",
+			"class": "event-warning",
+			"start": "1407848400000",
+			"end":   "1407859200000"
+		},
+		{
+			"id": "300",
+			"title": "Long VMUG Webinar Example PM",
+			"url": "https://vmug-events.webex.com/mw0401l/mywebex/default.do?nomenu=true&siteurl=vmug-events&service=6&rnd=0.22240610651555404&main_url=https%3A%2F%2Fvmug-events.webex.com%2Fec0701l%2Feventcenter%2Fevent%2FeventAction.do%3FtheAction%3Ddetail%26confViewID%3D1749628482%26%26MK%3D667500023%26%26%26siteurl%3Dvmug-events",
+			"class": "event-success",
+			"start": "1407848400000",
+			"end":   "1408500000000"
+		},
+		{
+			"id": "304",
+			"title": "Long VMUG Webinar Example AM",
+			"url": "https://vmug-events.webex.com/mw0401l/mywebex/default.do?nomenu=true&siteurl=vmug-events&service=6&rnd=0.22240610651555404&main_url=https%3A%2F%2Fvmug-events.webex.com%2Fec0701l%2Feventcenter%2Fevent%2FeventAction.do%3FtheAction%3Ddetail%26confViewID%3D1749628482%26%26MK%3D667500023%26%26%26siteurl%3Dvmug-events",
+			"class": "event-invertse",
+			"start": "1407848400000",
+			"end":   "1408456800000"
+		},
+		{
+			"id": "334",
+			"title": "VMUG Webinar Example PM",
+			"url": "https://vmug-events.webex.com/mw0401l/mywebex/default.do?nomenu=true&siteurl=vmug-events&service=6&rnd=0.22240610651555404&main_url=https%3A%2F%2Fvmug-events.webex.com%2Fec0701l%2Feventcenter%2Fevent%2FeventAction.do%3FtheAction%3Ddetail%26confViewID%3D1749628482%26%26MK%3D667500023%26%26%26siteurl%3Dvmug-events",
+			"class": "event-important",
+			"start": "1407866400000",
+			"end":   "1407877200000"
+		},
+		{
+			"id": "334",
+			"title": "VMUG Webinar Example PM",
+			"url": "https://vmug-events.webex.com/mw0401l/mywebex/default.do?nomenu=true&siteurl=vmug-events&service=6&rnd=0.22240610651555404&main_url=https%3A%2F%2Fvmug-events.webex.com%2Fec0701l%2Feventcenter%2Fevent%2FeventAction.do%3FtheAction%3Ddetail%26confViewID%3D1749628482%26%26MK%3D667500023%26%26%26siteurl%3Dvmug-events",
+			"class": "event-important",
+			"start": "1407934800000",
+			"end":   "1407945600000"
+		}
+	]
+}
+
+
